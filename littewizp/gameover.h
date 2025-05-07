4c305290b1dac9a9884fde08141b2071cf64dfb1
tapypara <tkhoa115.nvh2vn@gmail.com>
2025-05-07 09:20:44 +0700

    Delete littewizp directory

--- littewizp/gameover.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_OVER_H
-#define GAME_OVER_H
-
-#include <SDL.h>
-void renderGameOver();
-void updateGameOver(SDL_Event& e,GameState& gameState);
-#endif
-
--- littewizp/pause.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAUSE_H
-#define PAUSE_H
-
-#include <SDL.h>
-
-void renderPause();
-void updatePause(SDL_Event& e,GameState& gameState);
-#endif
-
